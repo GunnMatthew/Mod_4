9045ab0cc88e5e8858a30fd04b2e42bc231c0739
Matthew Gunn <Gunnmatthewe@gmail.com>
2024-04-27 14:36:05 -0700

    Module 4 homework commit

---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+let weatherForecast = {
+    weather: [
+        {day : "Sunday", forecast: "Sunny"},
+        {day : "Monday", forecast: "Rain"},
+        {day : "Tuesday", forecast: "Partially Sunny"},
+        {day : "Wednesday", forecast: "Overcast"},
+        {day : "Thursday", forecast: "Overcast"},
+        {day : "Friday", forecast: "Rain"},
+        {day : "Saturday", forecast: "Rain"},
+    ],
+};
