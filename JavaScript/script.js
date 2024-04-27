bc290b9991562c1bc9522d971fe66b649fec16e2
Matthew Gunn <Gunnmatthewe@gmail.com>
2024-04-27 16:22:57 -0700

    Assignment completion push

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,30 @@
+// JSON Payload
 let weatherForecast = {
     weather: [
         {day : "Sunday", forecast: "Sunny"},
         {day : "Monday", forecast: "Rain"},
-        {day : "Tuesday", forecast: "Partially Sunny"},
+        {day : "Tuesday", forecast: "Partly Cloudy"},
         {day : "Wednesday", forecast: "Overcast"},
         {day : "Thursday", forecast: "Overcast"},
         {day : "Friday", forecast: "Rain"},
         {day : "Saturday", forecast: "Rain"},
     ],
 };
+
+const tableBody = document.getElementsByTagName("tbody")[0];
+
+// Loop through array and generate data on website
+for(i of weatherForecast.weather)
+{
+    const row = document.createElement("tr");
+    const cellDay = document.createElement("td");
+    const cellForecast = document.createElement("td");
+
+    cellDay.textContent = i.day;
+    cellForecast.textContent = i.forecast;
+
+    row.appendChild(cellDay);
+    row.appendChild(cellForecast);
+
+    tableBody.appendChild(row);
+}
